2a954ec3c92246530bad461bc12e271a10d042c8
jacksoze <kamonyojack@gmail.com>
2022-06-14 11:03:47 +0300

    Create some  arithmetic functions

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
+//create arithmetic fuctions
 function sum ([...theArgs]) {
     let theArgsSum = theArgs.reduce((number, total) =>
     {return total + number}
     , 0)
     return theArgsSum;
+}
+
+function subtract ([a,b]) {
+    return a - b;
+}
+
+function multiply ([...theArgs]) {
+    let answer = 1;
+  for (const element of theArgs) {
+    answer = element * answer;
+  }
+  return answer;
+}
+
+function divide ([a,b]) {
+    return a/b;
 }
